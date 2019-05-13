154049fc1a844a5392232ad55a454c9fdb82b946
Ricarda Erhart <ricarda.erhart@gmail.com>
2019-05-13 09:03:27 +0200

    solved functon 1 and 2

--- assignment2.py
+++ assignment2.py
@@ -2,96 +2,106 @@
 
 import vcf
 
-__author__ = 'XXX'
+__author__ = 'Ricarda Erhart'
 
 
 class Assignment2:
-    
+
     def __init__(self):
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
-        
+
 
     def get_average_quality_of_file(self):
-        '''
-        Get the average PHRED quality of all variants
-        :return:
-        '''    
-        print("TODO")
-        
-        
+        vcf_reader = vcf.Reader(open('chr22_new.vcf', 'r'))
+        sumqual=0
+        vcfs=0
+        for entry in vcf_reader:
+            sumqual+=entry.QUAL
+            vcfs+=1
+        average=sumqual/vcfs
+        print('Average Quality of File: ', average)
+
+
+
     def get_total_number_of_variants_of_file(self):
-        '''
-        Get the total number of variants
-        :return: total number of variants
-        '''
-        print("TODO")
-    
-    
+        vcf_reader = vcf.Reader(open('chr22_new.vcf', 'r'))
+        vcfs=0
+        for entry in vcf_reader:
+            vcfs+=1
+        print('Number of Variants in File: ', vcfs)
+
+
     def get_variant_caller_of_vcf(self):
+        #callsetnames=,HiSeqPE300xfreebayes
         '''
         Return the variant caller name
-        :return: 
+        :return:
         '''
         print("TODO")
-        
-        
+
+
     def get_human_reference_version(self):
+        #difficultregion=AllRepeats_lt51bp_gt95identity_merged,hg38_self_chain_withalts_gt10k
         '''
         Return the genome reference version
-        :return: 
+        :return:
         '''
         print("TODO")
-        
-        
+
+
     def get_number_of_indels(self):
         '''
         Return the number of identified INDELs
         :return:
         '''
         print("TODO")
-        
+
 
     def get_number_of_snvs(self):
         '''
         Return the number of SNVs
-        :return: 
+        :return:
         '''
         print("TODO")
-        
+
     def get_number_of_heterozygous_variants(self):
         '''
         Return the number of heterozygous variants
-        :return: 
+        :return:
         '''
         print("TODO")
-        
-    
+
+
     def merge_chrs_into_one_vcf(self):
         '''
         Creates one VCF containing all variants of chr21 and chr22
         :return:
         '''
         print("TODO")
-        
+
         print("Number of total variants")
-        
-    
+
+
     def print_summary(self):
         print("Print all results here")
-    
-    
+        self.get_average_quality_of_file()
+        self.get_total_number_of_variants_of_file()
+        #self.get_variant_caller_of_vcf()
+        #self.get_human_reference_version()
+        self.get_number_of_indels()
+        #self.get_number_of_snvs()
+        #self.get_number_of_heterozygous_variants()
+        #self.merge_chrs_into_one_vcf()
+
+
+
 def main():
     print("Assignment 2")
     assignment2 = Assignment2()
     assignment2.print_summary()
     print("Done with assignment 2")
-        
-        
+
+
 if __name__ == '__main__':
     main()
-   
-    
-
-
-
